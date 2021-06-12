4588b895bae9fa8b8c9dad2d9c5ce9106fcaf861
Baki OZTURK <22.exe.94@gmail.com>
2021-06-13 00:32:25 +0300

    finish

--- HavaDurumu/script.js
+++ HavaDurumu/script.js
@@ -9,7 +9,24 @@ const setQuery = (e) => {
 
 const getResult = (cityName) => {
     let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
-    console.log(query);
+
+    fetch(query)
+        .then(weather => {
+            return weather.json()
+        })
+        .then(displayResult)
+}
+
+const displayResult = (result) => {
+    console.log(result);
+    let city = document.querySelector('.city');
+    city.innerText = `${result.name} , ${result.sys.country}`;
+    let temp = document.querySelector('.temp');
+    temp.innerText = `${Math.round(result.main.temp)}°C`;
+    let desc = document.querySelector('.desc');
+    desc.innerText = result.weather[0].description;
+    let minmax = document.querySelector('.minmax');
+    minmax.innerText = `${Math.round(result.main.temp_min)} °C - ${Math.round(result.main.temp_max)} °C`;
 }
 
 const searchBox = document.getElementById('searchBox');
